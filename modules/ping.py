95577f080cfbd80cf9b4c892be4b72f83ee86f5f
ayushboyofficia <160393536+ayushboyofficia@users.noreply.github.com>
2025-05-31 08:44:39 +0530

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -25,9 +25,9 @@ from utils.misc import modules_help, prefix
 @Client.on_message(filters.command(["ping", "p"], prefix) & filters.me)
 async def ping(_, message: Message):
     start = perf_counter()
-    await message.edit("<b>Pong!</b>")
+    await message.edit("<b>JINDA HU SALE MOON USERBOT⚡!</b>")
     end = perf_counter()
-    await message.edit(f"<b>Pong! {round(end - start, 3)}s</b>")
+    await message.edit(f"<b>JINDA HU SALE MOON USERBOT⚡! {round(end - start, 3)}s</b>")
 
 
 modules_help["ping"] = {
